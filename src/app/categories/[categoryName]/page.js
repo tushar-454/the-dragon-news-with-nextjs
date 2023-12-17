1ba70c154d6741cb70315cb3eb77c2472fd6e371
tushar-454 <imtushar454@gmail.com>
2023-12-18 04:16:17 +0600

    add a news page and news show based on category item click from database

--- src/app/categories/[categoryName]/page.js
+++ src/app/categories/[categoryName]/page.js
@@ -1,7 +1,58 @@
-const DynamicNewsPage = ({ searchParams }) => {
+import getCategoryNews from '@/utils/getCategoryNews';
+import { Box, CardActionArea, Grid } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Image from 'next/image';
+
+const DynamicNewsPage = async ({ searchParams }) => {
+  const { data: categoryNews } = await getCategoryNews(searchParams.category);
+
   return (
     <div>
-      <h1>Dynamic News Page is right side {searchParams.category}</h1>
+      <h1 className='text-lg font-medium my-10'>
+        This is {searchParams.category}. Total Number of news{' '}
+        {categoryNews.length}
+      </h1>
+      <Box>
+        <Grid
+          container
+          rowSpacing={1}
+          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          className='my-10'
+        >
+          {categoryNews.map((item) => (
+            <Grid key={item.id} item xs={6}>
+              <Card className='w-full'>
+                <CardActionArea>
+                  <Image
+                    src={item.thumbnail_url}
+                    width={500}
+                    height={500}
+                    className='object-cover h-[300px]'
+                    alt='lnb'
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant='h6'>
+                      {item.title.length > 30
+                        ? item.title.slice(0, 30) + '...'
+                        : item.title}
+                    </Typography>
+                    <Typography color='text.secondary' className='my-3'>
+                      By {item.author.name} - {item.author.published_date}
+                    </Typography>
+                    <Typography variant='body2' color='text.secondary'>
+                      {item.details.length > 200
+                        ? item.details.slice(0, 200) + '...'
+                        : item.details}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </div>
   );
 };
