6ae5f4d366682b969af636980d4d8977c061cdfc
tushar-454 <imtushar454@gmail.com>
2023-12-18 01:41:07 +0600

    add a navbar seciton with nextjs and meterial ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Project setup</h1>
+      <h1>Main components</h1>
     </div>
   );
 }
---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,82 @@
+'use client';
+import logo from '@/assets/logo.svg';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import { IconButton, Stack } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const navItems = [
+  {
+    route: 'Home',
+    path: '/',
+  },
+  {
+    route: 'Pages',
+    path: '/pages',
+  },
+  {
+    route: 'Category',
+    path: '/category',
+  },
+  {
+    route: 'News',
+    path: '/news',
+  },
+  {
+    route: 'About',
+    path: '/about',
+  },
+  {
+    route: 'Contact',
+    path: '/contact',
+  },
+];
+
+function Navbar() {
+  return (
+    <AppBar position='static' className='bg-black'>
+      <Container maxWidth='xl'>
+        <Toolbar disableGutters>
+          <Image src={logo} width={100} height={100} alt='logo' />
+          <Box className='w-full text-center'>
+            {navItems.map((item, index) => (
+              <Link key={index} href={item.path} className='px-4'>
+                {item.route}
+              </Link>
+            ))}
+          </Box>
+          <Box
+            sx={{
+              '& svg': {
+                color: '#fff',
+              },
+            }}
+          >
+            <Stack direction={'row'}>
+              <IconButton>
+                <FacebookIcon />
+              </IconButton>
+              <IconButton>
+                <TwitterIcon />
+              </IconButton>
+              <IconButton>
+                <YouTubeIcon />
+              </IconButton>
+              <IconButton>
+                <LinkedInIcon />
+              </IconButton>
+            </Stack>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Navbar;
